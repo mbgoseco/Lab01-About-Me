8c3f515acfe60f6ecec9f31ecb93039fef6cd8ae
mbgoseco <mbgoseco@gmail.com>
2019-01-08 00:45:27 -0800

    Initial setup, method questions created.

---
+++ About-Me/About-Me/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace About_Me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int correct = 0;        // Counter for questions answered correctly
+            bool tryValid = false;      // Flag used to continue or break out of loops containing try/catch blocks until valid input is entered 
+            Console.WriteLine("Welcome to the About Me Quiz!");
+
+            // First question. Code will loop until no exception is thrown.
+            while(!tryValid)
+            {
+                try
+                {
+
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public static string FavColor(string color)
+        {
+
+        }
+
+        public static int Birthyear(int year)
+        {
+
+        }
+
+        public static string StatesVisted(string state)
+        {
+
+        }
+
+        public static string FavSmashChar(string name)
+        {
+
+        }
+
+        public static bool CakeOrPie(bool isCake)
+        {
+
+        }
+    }
+}
