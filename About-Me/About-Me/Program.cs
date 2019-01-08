44a4234edb91506ef17d75505d367e29b162b165
mbgoseco <mbgoseco@gmail.com>
2019-01-08 01:46:10 -0800

    Question 1 complete

--- About-Me/About-Me/Program.cs
+++ About-Me/About-Me/Program.cs
@@ -4,49 +4,65 @@ namespace About_Me
 {
     class Program
     {
+        static int correct = 0;        // Counter for questions answered correctly
+        static bool tryValid = false;      // Flag used to continue or break out of loops containing try/catch blocks until valid input is entered 
         static void Main(string[] args)
         {
-            int correct = 0;        // Counter for questions answered correctly
-            bool tryValid = false;      // Flag used to continue or break out of loops containing try/catch blocks until valid input is entered 
             Console.WriteLine("Welcome to the About Me Quiz!");
 
-            // First question. Code will loop until no exception is thrown.
-            while(!tryValid)
+            // Question 1. Code will loop until no exception is thrown
+            string q1 = "";
+            while (!tryValid)
             {
                 try
                 {
-
+                    tryValid = true;    // Flag flips to true so loop can be escaped UNLESS an exception is caught
+                    q1 = FavColor();     // Assigns q1 to the return value of the method given by user's input
                 }
-                catch
+                catch (Exception e)
                 {
-
+                    tryValid = false;   // Flag is set to false again on exception so loop will continue
+                    Console.WriteLine(e.Message);
+                    throw;
                 }
             }
+            if (q1.ToLower() == "yellow")
+            {
+                Console.WriteLine("Correct! I love the color yellow!");
+                correct++;      // correct answer counter increments when user input matches correct answer
+            } else
+            {
+                Console.WriteLine("Wrong!");
+            }
+
+             uint q2;
         }
 
-        public static string FavColor(string color)
+        public static string FavColor()
         {
-
+            Console.Write("What is my favorite color? ");
+            string ans = Console.ReadLine();
+            return ans;
         }
 
-        public static int Birthyear(int year)
-        {
+        //public static int Birthyear()
+        //{
 
-        }
+        //}
 
-        public static string StatesVisted(string state)
-        {
+        //public static string StatesVisted()
+        //{
 
-        }
+        //}
 
-        public static string FavSmashChar(string name)
-        {
+        //public static string FavSmashChar()
+        //{
 
-        }
+        //}
 
-        public static bool CakeOrPie(bool isCake)
-        {
+        //public static bool CakeOrPie(bool isCake)
+        //{
 
-        }
+        //}
     }
 }
